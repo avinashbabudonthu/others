2d0a2cb5f47c1a29b93d6b02eb041f7422088c22
Avinash Babu Donthu <avinashbabu.donthu@gmail.com>
2020-04-08 10:44:06 +0530

    others

--- drools/hello-world/src/main/java/hello/world/model/Student.java
+++ drools/hello-world/src/main/java/hello/world/model/Student.java
@@ -1,5 +1,7 @@
 package hello.world.model;
 
+import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +15,7 @@ public class Student {
 
 	private String name;
 	private Integer age;
+	private Date joiningDate;
 
 	public boolean isAgeGreaterThan21() {
 		return age > 21;
--- drools/hello-world/src/main/java/hello/world/repository/StudentRepository.java
+++ drools/hello-world/src/main/java/hello/world/repository/StudentRepository.java
@@ -1,6 +1,9 @@
 package hello.world.repository;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import hello.world.model.Student;
@@ -14,6 +17,13 @@ public class StudentRepository {
 		studentList.add(Student.builder().name("jill").age(20).build());
 		studentList.add(Student.builder().name("jack").age(13).build());
 		studentList.add(Student.builder().name("john").age(14).build());
+		studentList.add(Student.builder().age(14).build());
+
+		Date date = Date.from(LocalDate.now().plusDays(1).atStartOfDay(ZoneId.systemDefault()).toInstant());
+		studentList.add(Student.builder().name("jane").age(14).joiningDate(date).build());
+
+		Date date2 = Date.from(LocalDate.now().minusDays(1).atStartOfDay(ZoneId.systemDefault()).toInstant());
+		studentList.add(Student.builder().name("smith").age(14).joiningDate(date2).build());
 	}
 
 	public List<Student> findAllStudents() {
